44ea5d40400d068b4db5314ca3d23157a59161df
Mateus Vitor <mateusriograndense@gmail.com>
2020-08-04 13:27:37 -0300

    Desprendendo componente de formulário do controle de comentários.

---
+++ comments_app/src/components/Comments.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="container">
+    <h1>Comentários</h1>
+    <hr />
+    
+    <FormTodo v-on:add-todo="addComment"/>
+
+    <div class="list-group">
+      <div
+        class="list-group-item list-group-item-action flex-column align-items-start"
+        v-for="(comment, index) in allComments" v-bind:key="index"
+      >
+        <div class="d-flex w-100 justify-content-between">
+          <p class="mb-1">
+            Autor:
+            <strong>{{ comment.name }}</strong>
+          </p>
+        </div>
+        <p class="mb-1">{{ comment.message }}</p>
+        <div>
+          <a href="#" title="Excluir" v-on:click.prevent="removeComment(index)">Excluir</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import FormTodo from './FormTodo' 
+
+export default {
+  components:{
+      FormTodo
+  },
+  data() {
+    return {
+      comments: [],
+      name: "",
+      message: "",
+    };
+  },
+  methods: {
+    removeComment(index) {
+      this.comments.splice(index, 1);
+    },
+    addComment(comment){
+        this.comments.push(comment)
+    }
+  },
+  computed: {
+    allComments() {
+      return this.comments.map((comment) => ({
+        ...comment,
+        name: comment.name.trim() === "" ? "Anônimo" : comment.name,
+      }));
+    },
+  },
+};
+</script>
+            
---
+++ comments_app/src/components/FormTodo.vue
@@ -0,0 +1,46 @@
+<template>
+  <form action>
+    <div class="form-todo form-group">
+      <p>
+        <input type="email" class="form-control" name="name" placeholder="Nome" v-model="name" />
+      </p>
+      <p>
+        <textarea
+          class="form-control"
+          name="message"
+          rows="3"
+          placeholder="Comentário"
+          v-model="message"
+        ></textarea>
+      </p>
+      <button v-on:click="addComment" type="button" class="btn btn-primary">Comentar</button>
+    </div>
+  </form>
+</template>
+
+
+<script>
+export default {
+    data(){
+        return{
+            name:'',
+            message:''
+        }
+    },
+    methods : {
+        addComment() {
+            if (this.message.trim() === "") {
+                return;
+            }
+
+            this.$emit('add-todo',{
+                name: this.name,
+                message: this.message
+            })
+
+            this.name = "";
+            this.message = "";
+            }
+    }
+}
+</script>
